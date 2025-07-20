cbb4f24ff72fe8ac156fadd78e5304f93902a8b7
Subham Behera <subhambehera5547@gmail.com>
2025-07-20 16:36:17 +0530

    Updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      REACT_APP_API_KEY: process.env.REACT_APP_API_KEY
+    }
+  }
 })
